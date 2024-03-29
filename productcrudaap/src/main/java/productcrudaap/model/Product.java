8400652bdbaf954f45a01b6647ed592097e4fdb1
Vinay Verma <151910988+Er-vinay-v@users.noreply.github.com>
2024-03-29 12:44:00 +0530

    complete project uploaded here

---
+++ productcrudaap/src/main/java/productcrudaap/model/Product.java
@@ -0,0 +1,59 @@
+package productcrudaap.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String description;
+	private String name;
+	private String price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public Product(int id, String description, String name, String price) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.name = name;
+		this.price = price;
+	}
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", description=" + description + ", name=" + name + ", price=" + price + "]";
+	}
+	
+	
+	
+}
